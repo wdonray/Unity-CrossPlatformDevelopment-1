9d378b1fac271ff6bdf7d931846c31b7bb00d0fb
Matthew Williamson <chuwilliamson@gmail.com>
2017-04-26 18:22:27 -0500

    initial commit with walles scene

---
+++ Assets/Scripts/RenameTextToName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RenameTextToName : MonoBehaviour {
+
+	// Use this for initialization
+	[ContextMenu("Rename")]
+    void Rename()
+    {
+        GetComponent<Text>().text = name;
+    }
+
+}
---
+++ Assets/Scripts/RunChoiceBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunChoiceBehaviour : MonoBehaviour
+{    
+
+	public void Run()
+    {
+        Debug.Log("Run Choice Selected I am running....");
+    }
+	
+}
