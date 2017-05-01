d961313678dfea6b8bc88938467ee854e33768e6
Matthew Williamson <chuwilliamson@gmail.com>
2017-05-01 01:25:13 -0500

    particles

--- Assets/Scripts/CatFactory.cs
+++ Assets/Scripts/CatFactory.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class CatFactory : PrefabFactory
 {
     public int maxCats = 5;
-    public float timer = 2f;
-
+    public float countdown = 2f;
+    public float ttl = 5f;
+    public float forceFactor = 1f;
+    public float torqueFactor = 1f;
     private void Start()
     {
         objectPool = new List<GameObject>();
@@ -17,21 +19,37 @@ public class CatFactory : PrefabFactory
         var go = Instantiate(prefab, transform);
         go.transform.localPosition = Vector3.zero;
         objectPool.Add(go);
-        go.GetComponent<Rigidbody2D>().AddForce(new Vector2(5f, 5f), ForceMode2D.Force);
+        go.GetComponent<Rigidbody2D>().AddForce(new Vector2(.5f, .5f) * forceFactor, ForceMode2D.Force);
+        go.GetComponent<Rigidbody2D>().AddTorque(10f * torqueFactor, ForceMode2D.Impulse);
+        var timercomponent = go.AddComponent<DestroyOnTimer>();
+        timercomponent.Time = 10f;
+        timercomponent.onDestroyed.AddListener(RemoveFromPool);
+        
     }
 
     private void Update()
     {
         if(objectPool.Count > maxCats)
             return;
-        if(timer <= 0)
+        if(countdown <= 0)
         {
             SpawnPrefab();
-            timer = 2f;
+            countdown = ttl;
         }
         else
         {
-            timer -= Time.deltaTime;
+            countdown -= Time.deltaTime;
         }
+    }
+
+
+    protected override void AddToPool(GameObject go)
+    {
+        objectPool.Add(go);
+    }
+
+    protected override void RemoveFromPool(GameObject go)
+    {
+        objectPool.Remove(go);
     }
 }
--- Assets/Scripts/PrefabFactory.cs
+++ Assets/Scripts/PrefabFactory.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public abstract class PrefabFactory : MonoBehaviour {
 
     [SerializeField]protected GameObject prefab;
-    protected List<GameObject> objectPool;
-    protected abstract void SpawnPrefab();	
+    [SerializeField]protected List<GameObject> objectPool;
+    protected abstract void SpawnPrefab();
+    protected abstract void AddToPool(GameObject go);
+    protected abstract void RemoveFromPool(GameObject go);
 }
