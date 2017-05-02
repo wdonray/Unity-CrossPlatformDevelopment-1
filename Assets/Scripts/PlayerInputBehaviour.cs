a7b0d0c8de73b0d327e281ca2f935a50c1dae2e2
Matthew Williamson <chuwilliamson@gmail.com>
2017-05-01 23:15:53 -0500

    split

--- Assets/Scripts/PlayerInputBehaviour.cs
+++ Assets/Scripts/PlayerInputBehaviour.cs
@@ -4,97 +4,100 @@
 public class PlayerInputBehaviour : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
-    private Animator _animator;
-
+    private Animator m_anim;
+    static readonly int CROUCHING = Animator.StringToHash("crouch");
     public float speed = 15f;
     public Vector3 velocity;
     public float scaleF;
     public Vector3 startScale;
     public float jumpPower = 600f;
     public bool blockinput = false;
-
+    public bool crouching = false;
+    public bool jumping = false;
+    public bool grounded = false;
+    
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
+        m_anim = GetComponent<Animator>();
         scaleF = transform.localScale.x;
         
     }
-
-    public Vector3 Move
-    {
-        get; set;
-    }
-    
+    float leftfootheight;
     private void Update()
     {
         if(blockinput)
             return;
+        
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
 
-        Move = new Vector3(h, v, 0);
+        var move = new Vector3(h, v, 0);
 
-        var dir = Move.normalized;
+        crouching = v < 0;
+        m_anim.SetBool(CROUCHING, crouching);       
+
+        var dir = move.normalized;
 
         velocity = dir * speed;
 
         var dot = Vector3.Dot(dir, transform.right);
+
         if(_rigidbody2D.velocity.magnitude > 0)
         {
             if(dot < 0)
                 SetScaleX(transform, -scaleF);
-            else if(dot > 0)
+            if(dot > 0)
                 SetScaleX(transform, scaleF);
         }
-        
 
-        _rigidbody2D.velocity = new Vector3(Move.x * speed, _rigidbody2D.velocity.y, 0);
+        _rigidbody2D.velocity = new Vector3(move.x * speed, _rigidbody2D.velocity.y, 0);
+    }
 
-        
+    public bool Move(Vector3 velocity, bool grounded, bool crouching, bool jumping)
+    {        
+        return true;
     }
 
     private void FixedUpdate()
-    {
+    {       
         if(Input.GetButtonDown("Fire1"))
         {
-            _animator.SetTrigger("kick");
+            m_anim.SetTrigger("kick");
             blockinput = true;
         }
             
-        if(Input.GetButtonDown("Fire2"))
-        {
-
-        }
+        if(Input.GetButtonDown("Fire2")){}
             
 
         if(Input.GetButtonDown("Fire3"))
         {
-            _animator.SetTrigger("punch");        //x key            
+            m_anim.SetTrigger("punch");        //x key            
             blockinput = true;
         }
             
         if(Input.GetAxis("Slide") > 0)
         {         
-            _animator.SetTrigger("slide");   //r trigger axis 10
+            m_anim.SetTrigger("slide");   //r trigger axis 10
         }
 
         if(Input.GetAxis("Block") > 0)
             print("Block" + " :: " + Input.GetAxis("Block"));
         if(Input.GetButtonDown("Jump"))
         {
-            _animator.SetTrigger("jump");
-            _rigidbody2D.AddForce(new Vector2(0, jumpPower));
+            m_anim.SetBool("jump", true);
+            //if(!m_anim.GetBool("crouch"))
+            //    _rigidbody2D.AddForce(new Vector2(0, jumpPower));
         }
-
     }
     
     private void OnAnimatorMove()
     {
         blockinput = true;
-        transform.position = _animator.rootPosition;
-        if(_animator.deltaPosition.magnitude < .01f)
+        transform.position = m_anim.rootPosition;
+        if(m_anim.deltaPosition.magnitude < .01f)
             blockinput = false;
+        leftfootheight = m_anim.leftFeetBottomHeight;
     }
 
     public static void SetScaleX(Transform t, float val)
