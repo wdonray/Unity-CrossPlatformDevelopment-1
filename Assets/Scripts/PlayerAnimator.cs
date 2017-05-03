d1ad1ef826e916eab548a5df9335e87d07c24b21
Matthew Williamson <chuwilliamson@gmail.com>
2017-05-02 22:30:08 -0500

    swing the sword and attach it

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,17 +4,97 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-    private Animator anim;   
-    private PlayerInputBehaviour playerInput;
+    private Animator m_anim;
 
+    static readonly int KICK = Animator.StringToHash("kick");
+    static readonly int BLOCK = Animator.StringToHash("block");
+    static readonly int SLIDE = Animator.StringToHash("slide");
+    static readonly int SWING = Animator.StringToHash("swing");
+    static readonly int PUNCH = Animator.StringToHash("punch");
+    static readonly int JUMP = Animator.StringToHash("jump");
+    static readonly int CROUCH = Animator.StringToHash("crouch");
+    static readonly int SPEED = Animator.StringToHash("speed");
+    static readonly int SWINGEXIT = Animator.StringToHash("swing_exit");
+
+    public GameObject weapon;
+    public GameObject shield;
+    public Transform wrist_right;
+    public Transform wrist_left;
+    public Transform spine;
+    public Vector3 weapon_start_pos;
+    public Quaternion weapon_start_rot;
     private void Start()
     {
-        anim = GetComponent<Animator>();
-        playerInput = GetComponent<PlayerInputBehaviour>();
+        m_anim = GetComponent<Animator>();
+        weapon_start_pos = weapon.transform.localPosition;
+        weapon_start_rot = weapon.transform.localRotation;
     }
 
+    public static bool isBlocking
+    {
+        get { return Input.GetButton("LeftBumper"); }
+    }
+    static bool isCrouching
+    {
+        get { return Input.GetAxis("Vertical") < 0; }
+    }
+
+    void AttachWeapon(GameObject weapon)
+    {
+        weapon.transform.SetParent(wrist_left.transform);       
+        weapon.transform.localPosition = Vector3.zero;
+        weapon.transform.localRotation = Quaternion.identity;
+        PlayerInputBehaviour.SetScaleX(weapon.transform, 7f);
+    }
+
+    void ResetWeapon(GameObject weapon)
+    {        
+        weapon.transform.SetParent(spine.transform);
+        weapon.transform.localPosition = weapon_start_pos;
+        weapon.transform.localRotation = Quaternion.identity;
+        PlayerInputBehaviour.SetScaleX(weapon.transform, -7f);
+
+    }
     private void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            m_anim.SetTrigger(KICK);
+        }
+
+        if(Input.GetButtonDown("Fire2"))
+        {
+            //parent            
+
+            //reset the rot then scale
+            m_anim.SetTrigger(SWING);        //x key          
+        }
+
+        if(Input.GetButtonDown("Fire3"))
+        {
+            m_anim.SetTrigger(PUNCH);        //x key            
+        }
+
+        if(Input.GetAxis("Slide") > 0)
+        {
+            m_anim.SetTrigger(SLIDE);   //r trigger axis 10
+        }
+
+        if(Input.GetButton("LeftBumper"))
+        {
+            m_anim.SetTrigger(BLOCK);
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            m_anim.SetTrigger(JUMP);
+        }
+
+        if(m_anim.GetBool("swinging"))
+            AttachWeapon(weapon);
+        else
+            ResetWeapon(weapon);
+        m_anim.SetBool(BLOCK, isBlocking);
+        m_anim.SetBool(CROUCH, isCrouching);
     }
 }
--- Assets/Scripts/PlayerInputBehaviour.cs
+++ Assets/Scripts/PlayerInputBehaviour.cs
@@ -5,100 +5,60 @@ public class PlayerInputBehaviour : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
     private Animator m_anim;
-    static readonly int CROUCHING = Animator.StringToHash("crouch");
     public float speed = 15f;
     public Vector3 velocity;
     public float scaleF;
     public Vector3 startScale;
     public float jumpPower = 600f;
-    public bool blockinput = false;
-    public bool crouching = false;
-    public bool jumping = false;
-    public bool grounded = false;
-    
+    public bool _rootMotion = false;
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         m_anim = GetComponent<Animator>();
         scaleF = transform.localScale.x;
-        
     }
-    float leftfootheight;
+
     private void Update()
     {
-        if(blockinput)
+        if(_rootMotion)
             return;
-        
+
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
 
         var move = new Vector3(h, v, 0);
-
-        crouching = v < 0;
-        m_anim.SetBool(CROUCHING, crouching);       
-
         var dir = move.normalized;
+
+        if(PlayerAnimator.isBlocking)
+            speed = 2.5f;
+        else
+            speed = 15f;
 
         velocity = dir * speed;
 
-        var dot = Vector3.Dot(dir, transform.right);
-
         if(_rigidbody2D.velocity.magnitude > 0)
         {
+            var dot = Vector3.Dot(dir, transform.right);
+
             if(dot < 0)
                 SetScaleX(transform, -scaleF);
             if(dot > 0)
                 SetScaleX(transform, scaleF);
         }
 
+       
+
         _rigidbody2D.velocity = new Vector3(move.x * speed, _rigidbody2D.velocity.y, 0);
+
     }
 
-    public bool Move(Vector3 velocity, bool grounded, bool crouching, bool jumping)
-    {        
-        return true;
-    }
-
-    private void FixedUpdate()
-    {       
-        if(Input.GetButtonDown("Fire1"))
-        {
-            m_anim.SetTrigger("kick");
-            blockinput = true;
-        }
-            
-        if(Input.GetButtonDown("Fire2")){}
-            
-
-        if(Input.GetButtonDown("Fire3"))
-        {
-            m_anim.SetTrigger("punch");        //x key            
-            blockinput = true;
-        }
-            
-        if(Input.GetAxis("Slide") > 0)
-        {         
-            m_anim.SetTrigger("slide");   //r trigger axis 10
-        }
-
-        if(Input.GetAxis("Block") > 0)
-            print("Block" + " :: " + Input.GetAxis("Block"));
-        if(Input.GetButtonDown("Jump"))
-        {
-            m_anim.SetBool("jump", true);
-            //if(!m_anim.GetBool("crouch"))
-            //    _rigidbody2D.AddForce(new Vector2(0, jumpPower));
-        }
-    }
-    
     private void OnAnimatorMove()
     {
-        blockinput = true;
         transform.position = m_anim.rootPosition;
-        if(m_anim.deltaPosition.magnitude < .01f)
-            blockinput = false;
-        leftfootheight = m_anim.leftFeetBottomHeight;
+        _rootMotion = (m_anim.deltaPosition.magnitude > 0);
     }
+
 
     public static void SetScaleX(Transform t, float val)
     {
