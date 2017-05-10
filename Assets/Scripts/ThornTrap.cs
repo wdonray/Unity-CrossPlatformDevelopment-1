561a3bc8ec2984017ecfdbede228cba898c6cc31
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-05-10 14:56:47 -0500

    This may break stuff

--- Assets/Scripts/ThornTrap.cs
+++ Assets/Scripts/ThornTrap.cs
@@ -20,6 +20,7 @@ public class ThornTrap : MonoBehaviour
         if(collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("collide with player");
+            Debug.Log(collision.gameObject.name);
             collision.gameObject.GetComponent<PlayerBehaviour>().ModifyStat(RUNTIME_MOD.EffectedStatType.ToString(), RUNTIME_MOD.TheMod);
             Destroy(gameObject);
         }
