360e4266b8066637cc547106b199f2d6b2785d07
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-05-12 17:29:22 -0500

    Change Sprite

--- Assets/Scripts/ShieldBehavior.cs
+++ Assets/Scripts/ShieldBehavior.cs
@@ -18,8 +18,7 @@ public class ShieldBehavior : MonoBehaviour
         CurrentShield = _ShieldConfig;
 
         MyColliderOffset = GetComponent<BoxCollider2D>().offset;
-        MyColliderSize = GetComponent<BoxCollider2D>().size;
-        GetComponent<SpriteRenderer>().sprite = _ShieldConfig.sprite;
+        MyColliderSize = GetComponent<BoxCollider2D>().size;    
     }    
 
     public void DoBlock(GameObject go)
